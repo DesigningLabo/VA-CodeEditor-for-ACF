9291f357f49077c86d63ec39a2eee532d6d89c30
KUCKLU <kucklu.xxx.happydays@gmail.com>
2014-03-12 17:18:06 +0900

    no message

--- va-codeeditor-for-acf.php
+++ va-codeeditor-for-acf.php
@@ -1,8 +1,8 @@
 <?php
 defined('ABSPATH') or die();
 /*
-Plugin Name: VA CodeMirror for ACF
-Description: CodeMirror to textarea filled for Advanced Custom Fields.
+Plugin Name: VA CodeEditor for ACF
+Description: CodeMirror and Emmet to textarea filled for Advanced Custom Fields.
 Version: 1.0.0
 Plugin URI: http://visualive.jp/wordpress/plugins/va_codemirroe_cssmode_for_acf/
 Author: KUCKLU
@@ -18,8 +18,8 @@ define('VACMACF_VARSION', $vacmcaf_data['ver']);
 define('VACMACF_LANGS', $vacmcaf_data['langs']);
 define('VACMACF_THEME', 'visualive');
 
-if ( class_exists( 'acf_field' ) AND ! class_exists( 'VA_CodeMirror_For_ACF' ) ) :
-class VA_CodeMirror_For_ACF {
+if ( class_exists( 'acf_field' ) AND ! class_exists( 'VA_CodeEditor_For_ACF' ) ) :
+class VA_CodeEditor_For_ACF {
 	private $version = '';
 	private $langs   = '';
 
@@ -37,10 +37,10 @@ class VA_CodeMirror_For_ACF {
 	function include_before_theme() {
 		// admin only includes
 		if( is_admin() ) {
-			include_once( VACMACF_PATH . '/include/va-codemirror-mixedmode-for-acf.php' );
-			include_once( VACMACF_PATH . '/include/va-codemirror-htmlmode-for-acf.php' );
-			include_once( VACMACF_PATH . '/include/va-codemirror-cssmode-for-acf.php' );
-			include_once( VACMACF_PATH . '/include/va-codemirror-jsmode-for-acf.php' );
+			include_once( VACMACF_PATH . '/include/editor-mixedmode.php' );
+			include_once( VACMACF_PATH . '/include/editor-htmlmode.php' );
+			include_once( VACMACF_PATH . '/include/editor-cssmode.php' );
+			include_once( VACMACF_PATH . '/include/editor-jsmode.php' );
 		}
 	}
 
@@ -48,5 +48,5 @@ class VA_CodeMirror_For_ACF {
 		wp_enqueue_style('source-code-pro', '//fonts.googleapis.com/css?family=Source+Code+Pro:200,300,400,500,600,700,900', array(), $this->version, 'all' );
 	}
 }
-new VA_CodeMirror_For_ACF();
+new VA_CodeEditor_For_ACF();
 endif;
